e9ece14034456d6be55ef2a7f0a21d1a0edf55b0
pkc-3 <pkc_3@naver.com>
2021-06-29 18:05:13 +0900

    Ch01 R 설치와 개요

---
+++ Ch01/1_2_DataType.R
@@ -0,0 +1,49 @@
+# 날짜 : 2021/06/28
+# 이름 : 고현석
+# 내용 : Ch01. R설치와 개요 - 자료형 교재 p41
+
+# 숫자형
+num1 <- 1
+num2 <- 2
+num3 <- 3.14
+num1
+num2;num3
+
+# 논리형
+bool1 <- TRUE
+bool2 <- FALSE
+bool1;bool2
+
+
+# 문자형
+str1 <- "Hello"
+str2 <- 'Welcome'
+str1
+str2
+
+# 특수형
+data1 <- NULL #초기값을 의미
+data2 <- NA   #결측값, Not Available
+data3 <- NaN  #숫자가 아닌 값, Not a Number
+
+data1
+data2
+data3
+
+
+#교재 p42 실습 - 스칼라 변수 사용 예
+
+#교재 p43 실습 - 자료형 확인
+#교재 p43 실습 - 문자 원소를 숫자 원소로 형 변환하기기
+
+#교재 p44 실습 - 스칼라 변수의 자료형과 자료구조 확인
+
+#교재 p45 실습 - 문자벡터와 그래프 생성
+#교재 p45 실습 - as.factor() 함수를 이용하여 요인형 변환
+
+#교재 p46 실습 - Factor형 변수로 차트 그리기
+
+
+
+
+
